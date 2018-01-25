ba323835502c5d4cdf26a82db8a3ef5bda22e538
David Moran <ddavidmorang@gmail.com>
2018-01-25 20:49:13 +0100

    Remove deprecated scripts & add participation conditions

--- views/main.vue
+++ views/main.vue
@@ -1,11 +1,17 @@
 <template> 
   <div>
     <div class="col-10 offset-1">
+      <h3>Para participar en esta tabla</h3>
+      <ul>
+        <li>Rellenar el formulario en: <a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLScUuImEQB3HAYjOVEN_n3iOhlIrq41XxRpZeMUfN2RiwctElA/viewform">Google</a></li>
+        <li>Esperar hasta que uno de los contribuyentes te agregue manualmente.</li>
+        <li>Después, todos los resultados se actualizarán cada 20 minutos.</li>
+      </ul>
       <table class="table table-bordered table-hover" border="1">
         <thead>
             <td style="font-weight: bold; text-align: center">USUARIO</td>
             <td style="font-weight: bold; text-align: center" v-for="col of cols">
-              <a :href="col.url">{{col.code}}</a>
+              <a target="_blank" :href="col.url">{{col.code}}</a>
             </td>
         </thead>
         <tbody>
