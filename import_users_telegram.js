59585a3fda83457b47bda3e885a9297c04a5158f
isaaclo97 <i.lozanoo@alumnos.urjc.es>
2018-10-25 19:45:53 +0200

    Telegram updated

---
+++ import_users_telegram.js
@@ -0,0 +1,50 @@
+//const controller = require('./models/index');
+
+//TODO A NICE INTERFACE
+
+const USERSTG = [{
+  identifier: 'David Morán',
+  spoj_handler: 'david_8k',
+  aer_handler: '2226',
+}, {
+  identifier: 'Jakub Luczyn',
+  spoj_handler: 'jjluczyn',
+  aer_handler: '5095',
+}, {
+  identifier: 'Isaac Lozano Osorio',
+  spoj_handler: 'isaaclo97',
+  aer_handler: '4920',
+}, {
+  identifier: 'Iván Martín',
+  spoj_handler: 'las_mas_debugs',
+  aer_handler: '8640',
+},  {
+  identifier: 'David Mestanza',
+  spoj_handler: 'davidm_369',
+  aer_handler: '8923',
+},  {
+  identifier: 'Jorge Prieto',
+  spoj_handler: 'jorgeprg97',
+  aer_handler: '5139',
+},{
+  identifier: 'José Hernández',
+  spoj_handler: 'jose_he_ch',
+  aer_handler: '7257',
+}, {
+  identifier: 'Sergio Perez',
+  spoj_handler: 'n_tesla',
+  aer_handler: '2248',
+}, {
+  identifier: 'Juan David',
+  spoj_handler: 'jdavid18k',
+  aer_handler: '8950',
+} ];
+/*
+Promise.all(USERS.map(user => {
+  return controller.createUser(user);
+})).then(res => {
+  console.log(res);
+  console.log('DONE IMPORTING!');
+  process.exit(0);
+});
+*/
